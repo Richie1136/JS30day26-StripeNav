25e7f4de9b5a50aa80302aa6a3744297d3e8e16f
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-14 14:46:29 -0400

    Adding a classlist of trigger enter when the mouse event is of type mouse enter and then adding a class of trigger enter active after 150 ms and then remove those classes of mouse event of type mouseleave

--- main.js
+++ main.js
@@ -3,15 +3,20 @@ const background = document.querySelector('.dropdownbackground')
 const nav = document.querySelector('.top')
 
 function handleEnter(event) {
-  console.log("Hello")
+  this.classList.add('trigger-enter')
+  setTimeout(() => {
+    this.classList.add('trigger-enter-active')
+  }, 150);
 }
 
 function handleLeave(event) {
-  console.log("Leave")
+  this.classList.remove('trigger-enter')
+  setTimeout(() => {
+    this.classList.remove('trigger-enter-active')
+  }, 150);
 }
 
 triggers.forEach(trigger => {
   trigger.addEventListener('mouseenter', handleEnter)
   trigger.addEventListener('mouseleave', handleLeave)
-
 })
